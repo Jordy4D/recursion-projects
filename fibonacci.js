65ac063b65387b2651f154e900b2b2087f676bce
JordanAdams_MacBook <jsadams27@gmail.com>
2024-10-16 20:17:45 -0400

    starting on recursive version

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@ function fibs(num) {
     
     let arr = [0, 1]
 
-    for (let i = 0; i <= num; i++) {
+    for (let i = 0; i <= num ; i++) {
         let x = arr[i]
         let y = arr[i+1]
 
@@ -12,6 +12,34 @@ function fibs(num) {
     }
 
     console.log(arr)
+}
 
+function fibs2(num) {
+    let arr = []
+
+    let a = 0
+    let b = 1
+
+
+}
+
+function fibsRec(num) {
+    let arr = []
+    let a = 1;
+    let b = 0;
+    let temp;
+    arr.push(b)
+
+    if (arr.length === num) {return};
+
+    if (arr.length < num) {
+        temp = a;
+        a += b
+        b = temp
+        fibsRec(num - 1)
+        
+    }
+    
+    console.log(arr)
 
 }
